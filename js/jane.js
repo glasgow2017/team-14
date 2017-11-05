5373f2426c610d874961acfb2d5c3e1f2021bec3
verden11 <justinas.reigis@gmail.com>
2017-11-05 02:10:06 +0000

    add Jace persona object

---
+++ js/jane.js
@@ -0,0 +1,27 @@
+Jane = {
+  info: "Hi, I’m Jane, I’m 19 and I live in South Florida. Where are you from?",
+  mainQuestion1: "What education has Jane already gained?",
+  mainQuestion2: "How likely is it that Jane will get a college degree?",
+  mainQuestion3: "What is Jane’s relationship with the father of the child?",
+  ans1A: "primary",
+  ans1B: "secondary (high school)",
+  ans1C: "college",
+
+  ans2A: "2%",
+  ans2B: "45%",
+  ans2C: "92%",
+
+  ans3A: "We’re married",
+  ans3B: "We broke up",
+  ans3C: "I don’t know who the father is",
+
+  smallQuestion: "Nice! Is there anything you want to ask me?",
+
+  opt1: "How big is your family?",
+  opt2: "What do you do in your free time?",
+  opt3: "Do you like learning?",
+
+  opt1ans: "I live with my parents and I also have a lovely girl - Megan!",
+  opt2ans: "I like to go for walks with my little girl.",
+  opt3ans: "Yes! One day I want to become an accountant."
+};
